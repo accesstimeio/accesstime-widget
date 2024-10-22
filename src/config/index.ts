e0ec1d61d5a7cd674dc90bf604856c43972ee560
mehmetraufoguz <88249309+mehmetraufoguz@users.noreply.github.com>
2024-10-22 17:18:08 +0300

    chore: add api service
    update config constants
    add axios package

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,1 +1,16 @@
+import { Address, parseAbi } from "viem";
+
 export const SUPPORTED_CHAIN_IDS = [8453, 84532]; // base, baseSepolia
+
+export const FACTORY_ADDRESS: Address = "0x84Ec87B41272223755AdD5f2A8271290aD5d87f0";
+
+export const FACTORY_ABI = parseAbi([
+    'function deploymentDetails(address accessTime) view returns (bool status,uint256 id,bool includedExtraTime,bool includedPackageModule,string name,string description,string website)',
+]);
+
+export const ACCESTIME_ABI = parseAbi([
+    'function purchase(uint256 amount,address paymentToken)',
+    'function purchasePackage(uint256 amount,address paymentToken,uint256 packageID)',
+]);
+
+export const API_URL = "https://api.accesstime.io";
---
+++ src/services/api.ts
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import { Address } from 'viem';
+import { API_URL } from '../config';
+
+const api = axios.create({
+	baseURL: API_URL,
+});
+
+export type ProjectResponseDto = {
+	id: Address;
+	extraTimes: string[];
+	removedExtraTimes: string[];
+	nextOwner: Address;
+	owner: Address;
+	packages: string[];
+	removedPackages: string[];
+	paused: boolean;
+	paymentMethods: Address[];
+	prevOwner: Address;
+};
+
+export const getProject = async (chainId: number, id: number): Promise<ProjectResponseDto> => {
+	const { data } = await api.get(`/project/${chainId}/${id}`);
+	return data;
+};
