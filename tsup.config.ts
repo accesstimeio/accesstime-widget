025df45ed8702ea68d2628c8c63c94aed6cc2997
mehmetraufoguz <88249309+mehmetraufoguz@users.noreply.github.com>
2024-12-21 16:47:38 +0300

    chore: update tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-	entry: ['src/index.tsx'],
-	format: ['cjs', 'esm'],
-	dts: true,
-	sourcemap: true,
-	minify: true,
-	clean: true,
-	external: ['react', 'react-dom'],
+    entry: ['src/**/*.ts', 'src/**/*.tsx'],
+    format: ['cjs', 'esm'],
+    dts: true,
+    clean: true,
+    splitting: true,
+    treeshake: true,
+    bundle: false,
+    external: ['react', 'react-dom'],
 });
